5312b7358b2089fb51c11f833a48de045ab30585
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2025-03-17 12:00:58 -0300

    Adicionando primeira foto e alterando CSS do flexbox

--- javascript/script2.js
+++ javascript/script2.js
@@ -2,14 +2,33 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear();
 
-    var fanoA = document.getElementById('txtano')
-    var fanoB = Number.parseInt(fanoA)
+    var fano = document.getElementById('txtano')
 
     var resposta = document.getElementById('resp')
 
-    if(fanoA.value.length == 0){
+    if(fano.value.length == 0){
         window.alert('ERRO, digite ano')
     }
     else{
+        var sex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+
+        var imagem = document.createElement('img')
+        imagem.setAttribute('id','foto')
+
+        if (sex[0].checked){
+            if(idade <= 3){
+                imagem.setAttribute('src','imagens/bebe.jpg')
+            }
+            else if (idade <= 12){}
+            else if(idade <= 21){}
+            else if(idade <= 60){}
+            else{}
+            resposta.innerHTML = `Homem de ${idade} anos`
+            resposta.appendChild(imagem)
+        }
+        else{
+            resposta.innerHTML = `Mulher de ${idade} anos`
+        }
     }
 }
