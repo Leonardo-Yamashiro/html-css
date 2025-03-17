ecad76794593fc828f5c6dfae4b8b057ff9f1095
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2025-03-17 10:47:39 -0300

    Organizando as imagens, e começando exercício de verificação de idade

--- javascript/script1.js
+++ javascript/script1.js
@@ -7,15 +7,15 @@ function carregar(){
     var hora = data.getHours()
     mensagem.innerText = `Agora são ${hora} horas.`
     if (hora>0 && hora<=12){
-        imagem.src = 'manha.png'
+        imagem.src = 'imagens/manha.png'
         fundo.style.background = 'orange'
     }
     else if (hora>12 && hora<=18){
-        imagem.src = 'tarde.png'
+        imagem.src = 'imagens/tarde.png'
         fundo.style.background = 'lightblue'
     }
     else{
-        imagem.src = 'noite.png'
+        imagem.src = 'imagens/noite.png'
         fundo.style.background = 'purple'
     }
 }
