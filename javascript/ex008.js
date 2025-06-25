3cec2b7f9abfafc78a7f524ce6a72e24511b232b
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2025-06-25 09:54:38 -0300

    adicionando limite de velocidade ajustavel

--- javascript/ex008.js
+++ javascript/ex008.js
@@ -1,12 +1,14 @@
 console.log("Teste de funcionamento")
-var vel = 60
+var vel = 45
+var limite = 60
 console.log("Analise de Velocidade")
-if (vel== 60){
+if (vel == limite){
     console.log("Limite de velocidade: " + vel)
 }
-else if (vel > 60){
+else if (vel > limite){
     console.log("ALERTA de velocidade: " + vel)
+    console.log("LIMITE: " + limite)
 }
 else{
-    console.log("Abaixo do limite: " + vel)
+    console.log("Abaixo do limite: " + limite)
 }
