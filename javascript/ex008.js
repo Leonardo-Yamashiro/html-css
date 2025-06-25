f5c20bb20e89f9a0284c32b5ed4f280a98964b99
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2025-06-25 09:47:43 -0300

    teste de criação de um Node.javascript

---
+++ javascript/ex008.js
@@ -0,0 +1,12 @@
+console.log("Teste de funcionamento")
+var vel = 60
+console.log("Analise de Velocidade")
+if (vel== 60){
+    console.log("Limite de velocidade: " + vel)
+}
+else if (vel > 60){
+    console.log("ALERTA de velocidade: " + vel)
+}
+else{
+    console.log("Abaixo do limite: " + vel)
+}
