8b609fb261d1a61c255544db23d1e468de17d0b5
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2025-06-28 10:55:47 -0300

    contador funcionando

--- javascript/script3.js
+++ javascript/script3.js
@@ -1,18 +1,38 @@
 function contar(){
-    var inicio = document.getElementById("iinicio")
+    var ini = document.getElementById("iinicio")
     var fim = document.getElementById("ifim")
     var passo = document.getElementById("ipasso")
     var resp = document.getElementById("resp")
 
-    if(inicio == null || fim == null){
-        window.alert("ERRO")
+    if(ini.value.length == 0 || fim.value.length == 0){
+        window.alert("ERRO, faltam dados!")
     }
+    else{
+        resp.innerHTML = "Contando... "
 
-    if (passo == null){
-        passo = inicio
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+
+        if (passo.value.length == 0 ){
+            var p = 1
+        }
+        else{
+            var p = Number(passo.value)
+        }
+
+        if(i > f){
+            for(var c = i; c >= f; c -= p){
+                resp.innerHTML += c + " > "
+            }
+        }
+        else if (i == f){
+            resp.innerHTML += c 
+        }
+        else{
+            for(var c = i; c <= f; c += p){
+                resp.innerHTML += c + " > "
+            }
+        }
+        resp.innerHTML += "FIM \u{1F600}"
     }
-
-    if(inicio > fim){}
-    else if (inicio == fim){}
-    else{}
 }
