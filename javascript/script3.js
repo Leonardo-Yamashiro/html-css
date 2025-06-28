a77b07dc38e83e645ca082c37c020ae0aa88e40c
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2025-06-28 09:41:46 -0300

    organização do codigo inicial

---
+++ javascript/script3.js
@@ -0,0 +1,18 @@
+function contar(){
+    var inicio = document.getElementById("iinicio")
+    var fim = document.getElementById("ifim")
+    var passo = document.getElementById("ipasso")
+    var resp = document.getElementById("resp")
+
+    if(inicio == null || fim == null){
+        window.alert("ERRO")
+    }
+
+    if (passo == null){
+        passo = inicio
+    }
+
+    if(inicio > fim){}
+    else if (inicio == fim){}
+    else{}
+}
