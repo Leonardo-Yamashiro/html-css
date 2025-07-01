c66317668abf7a0c9987ecebc60d33d8b4321b53
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2025-07-01 12:36:27 -0300

    verificador de lista funcionando

--- javascript/script7FINAL.js
+++ javascript/script7FINAL.js
@@ -1,12 +1,22 @@
-function add(){
-    var valor = document.getElementById("inumber")
-    var lista = document.getElementById("ilista")
+var vet = []
+var valor = document.getElementById("inumber")
+var lista = document.getElementById("ilista")
+var resposta = document.getElementById("iresposta")
 
+function inLista(n,l){
+    if(l.indexOf(n) == -1){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function add(){
     if(valor.value.length == 0){
         window.alert("ERRO: valor nao foi inserido.")
     }
     else{
-        var vet = []
         var num = Number(valor.value)
         if(num < 1 || num > 100){
             window.alert("ERRO: valor invalido")
@@ -14,10 +24,16 @@ function add(){
         else{
             if(vet.length == 0){
                 vet.push(num)
-                lista.innerHTML += "Adicionado: " + num + "<br>"
+                lista.innerHTML += "<option value=" + " " + "> Adicionado " + num +"</option>"
             }
             else{
-                window.alert("ERRO: valor ja esta presente na lista")
+                if(inLista(num,vet)){
+                    vet.push(num)
+                    lista.innerHTML += "<option value=" + " " + "> Adicionado " + num +"</option>"
+                }
+                else{
+                    window.alert("ERRO: valor ja existe")
+                }
             }
         }
     }
