c72970a75c621bfee0da762d358248f9de8f903b
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2026-01-25 08:09:15 -0300

    Primeiro codigo PHP

---
+++ cursophp/ex000/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Codigo PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Titulo criado com PHP \u{1F30E} ";
+            print "Segundo titulo, usando print";
+        ?>
+    </h1>
+    <p>
+        texto de paragrafo
+    </p>
+    <p>
+        Olá, Mundo!
+    </p>
+</body>
+</html>
