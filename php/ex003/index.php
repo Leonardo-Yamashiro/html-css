e1d4b30e996f0e0ec6de375dadc06ef207c5a625
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2026-01-30 09:02:35 -0300

    Novo codigo PHP, aprendendo variavel e constantes

---
+++ php/ex003/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variavel e Constante</title>
+</head>
+<body>
+    <h1>Variavel e Constante</h1>
+    <?php
+        $nome = "Leo";
+        $sobrenome = "S. Y.";
+        
+        $sobrenome = "Yamashiro";
+
+        const PAIS = "Brasil";
+        
+        echo "Muito prazer em conhecer $nome $sobrenome! Voce mora no " . PAIS ;
+    ?>
+</body>
+</html>
