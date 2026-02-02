c4189a7a78b396735d614faeb2f804190164c7ec
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2026-02-02 09:23:49 -0300

    Aprendendo Vetor/Array e Class em PHP

--- php/ex004/index.php
+++ php/ex004/index.php
@@ -37,6 +37,18 @@
         echo "O valor de num é $num.  ";
         var_dump($num);
         echo "<p><br></p>";
+
+        $vet = [2 , 6.5 , 7 , 5 , "palavra"];
+        echo "O valor da posição 1 é $vet[1].  ";
+        var_dump($vet);
+        echo "<p><br></p>";
+
+        class Pessoa{
+            private string $nome;
+        }
+
+        $p = new Pessoa;
+        var_dump($p);
     ?>
 </body>
 </html>
