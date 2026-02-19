2a0514e4c91336345013e107d3fff7bef3099936
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2026-02-19 10:45:29 -0300

    Exercicio de expressao aritmetica

---
+++ php/ex006/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expressões Aritméticas</title>
+</head>
+<body>
+    <?php 
+    $res = 5 + 2 / 2;
+    echo "5 + 2 / 2 = $res";
+    ?>
+</body>
+</html>
