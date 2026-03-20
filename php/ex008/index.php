aa9aa30413c6e3a181f7473dfe87fe487b12365d
Leonardo Siqueira Yamashiro <lsyamashiro@gmail.com>
2026-03-20 10:31:15 -0300

    Novo exercicio, PHP interativo

---
+++ php/ex008/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 008</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Desafio 008</h1>
+    </header>
+    <section>
+        <?php
+            $valor1 = $_GET['numero1']??0;
+            $valor2 = $_GET['numero2']??0;
+            
+            $inicio = date("m-d-Y",strtotime("-7 days"));
+            $fim = date("m-d-Y");
+            
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+            $dados = json_decode(file_get_contents($url), true);
+            
+            $cota = $dados["value"][0]["cotacaoCompra"];
+        ?>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="get">
+            <label for="numero1">Coloque Valor em Reais:</label>
+            <input type="number" name="numero1" id="inumero1" value="0" step="0.001">
+            <label for="numero2">Coloque Valor Desejado em Dolares:</label>
+            <input type="number" name="numero2" id="inumero2" value="0" step="0.001">
+            
+            <input type="submit" value="Calcular" id="idenviar">
+
+            <?php
+                $real = $valor1/$cota;
+                $dolar = $valor2*$cota;
+                echo "<p>Valor obtido: " . number_format($real,2) . " com $valor1.</p>";
+                echo "<p>Valor necessario: " . number_format($dolar,2) . " para $valor2.</p>";
+            ?>
+        </form>
+    </section>
+
+    <footer>
+        <p>Site desenvolvido por Leonardo Siqueira Yamashiro</p>
+    </footer>
+</body>
+</html>
